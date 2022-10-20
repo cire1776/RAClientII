9836c1c56d02a53ff9160713ddc0cf59f555eaa9
Eric Russell <cire1776@gmail.com>
2022-10-20 06:47:26 -0400

    Initial Commit

---
+++ RAClientII/RAClientIIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RAClientIIApp.swift
+//  RAClientII
+//
+//  Created by Eric Russell on 10/20/22.
+//
+
+import SwiftUI
+
+@main
+struct RAClientIIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
