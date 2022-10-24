7f47335f6b586b97d8fc829b18c0606a23802a34
Eric Russell <cire1776@gmail.com>
2022-10-24 08:34:29 -0400

    Change RABackend_GameCommand to complete commands, rewrite toOrientation.

--- RAClientII/Extensions/Protobuff.swift
+++ RAClientII/Extensions/Protobuff.swift
@@ -1,20 +1,28 @@
 //
-//  Protobuff.swift
+//  ProtoBuff.swift
 //  RAClientII
 //
-//  Created by Eric Russell on 10/22/22.
+//  Created by Eric Russell on 10/24/22.
 //
 
 import Foundation
-import SwiftProtobuf
 
-extension RABackend_GameCommand {
+public extension RABackend_Orientation {
+    var toOrientation: Hexagon.Orientation {
+        self == .point ? .point : .flat
+    }
+}
+
+public extension RABackend_GameCommand {
     init(clientCommand: ClientCommand) {
         switch clientCommand {
-        case .connect:
-            self.command = .connect
+        case .nop,.wait(_):
+            break
         case .report:
             self.command = .report
+        case .connect:
+            self.command = .connect
+            self.stringParam = ""
         case .close:
             self.command = .close
         case .beginOperation:
@@ -23,41 +31,35 @@ extension RABackend_GameCommand {
             self.command = .cancelOperation
         case .command:
             self.command = .command
-        case .face(let facing):
+        case .face(facing: let facing):
             self.command = .face
             self.facing = UInt64(facing)
-        case .addWaypoint:
+        case .addWaypoint(destination: let destination, duration: let duration):
             self.command = .addWaypoint
+            self.addWaypointParams.position = RABackend_VenuePosition(position: destination)
+            self.addWaypointParams.for = duration
         case .abortMovement:
             self.command = .abortMovement
         case .abortLastWaypoint:
             self.command = .abortLastWaypoint
-        case .consume:
+        case .consume(itemID: let itemID):
             self.command = .consume
-        case .use:
+            self.itemID.id = itemID
+        case .use(itemID: let itemID):
             self.command = .use
-        case .drop:
+            self.itemID.id = itemID
+        case .drop(itemID: let itemID):
             self.command = .drop
-        case .pickup:
+            self.itemID.id = itemID
+        case .pickup(droppedItemID: let droppedItemID):
             self.command = .pickup
-        case .equip:
+            self.droppedItemID.id = droppedItemID
+        case .equip(itemID: let itemID):
             self.command = .equip
-        case .unequip:
+            self.itemID.id = itemID
+        case .unequip(itemID: let itemID):
             self.command = .unequip
-        default:
-            self.command = .nop
+            self.itemID.id = itemID
         }
-    }
-}
-
-extension RABackend_Orientation {
-    var toOrientation: Hexagon.Orientation {
-        self == .point ? .point : .flat
-    }
-}
-
-extension RABackend_Size {
-    var toCGSize: CGSize {
-        CGSize(width: self.width, height: self.height)
     }
 }
